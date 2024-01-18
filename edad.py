abecadafd5fb01e3ce74d104cfb5236ea71c1310
Arley <Arleycorreopa>
2024-01-18 07:11:32 -0500

    Se modficó con otra rama

--- edad.py
+++ edad.py
@@ -3,4 +3,4 @@ edad= int(input("ingrese por favor su edad"))
 if edad >=18:
     print(f"{nombre}, Eres mayor de edad")
 else:
-    print(f"{nombre}, Eres menor de edad")
+    print(" Eres menor de edad")
