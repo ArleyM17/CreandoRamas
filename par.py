bc03fdd17bdbc8517ea9ab37a40d8a6a121cae45
Arley <Arleycorreopa>
2024-01-18 07:14:28 -0500

    SE modifico archivo par

--- par.py
+++ par.py
@@ -2,4 +2,4 @@ numero =int(input("Ingrese un numero positivo: "))
 if numero %2==0:
     print(f"El numero {numero} es par")
 else:
-    print(f"El numero {numero} es impar")
+    print(f"El numero 23 es impar")
